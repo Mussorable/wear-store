b06d55157c271641e40fa8c342d2c360b2060e94
Oleh Diakov <redduck5601@gmail.com>
2023-06-06 22:17:01 +0200

    added rendering background and added new styles

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 interface CategoryProps {
   children: React.ReactNode;
+  imageURL: string;
 }
 
-const Category: React.FC<CategoryProps> = ({ children }) => {
+const Category: React.FC<CategoryProps> = ({ children, imageURL }) => {
   return (
     <div className="category-container">
-      <div className="background-image"></div>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageURL})`,
+        }}
+      />
       <div className="category-body-container">
         <h2>{children}</h2>
         <Link to={"/"}>Shop now</Link>
