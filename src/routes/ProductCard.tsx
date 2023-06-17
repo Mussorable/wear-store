b6b61f0aabe10cb3b871d00618ae69bf5fd7890e
Mussorable <redduck5601@gmail.com>
2023-06-17 12:19:18 +0200

    added preview and product cards, add product category type endpoint, styles

---
+++ src/routes/ProductCard.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Product from "../parts/Product";
+import { WearData } from "../redux-components/categoriesSlice";
+import { RootState } from "../redux-components/store";
+
+const ProductCard = () => {
+  const { category } = useParams();
+  const listOfProducts = useSelector(
+    (state: RootState) => state.categories.listOfProducts
+  );
+  const [products, setProducts] = useState([] as WearData[]);
+
+  useEffect(() => {
+    {
+      if (category) {
+        setProducts(listOfProducts[category]);
+      }
+    }
+  }, [category, listOfProducts]);
+
+  return (
+    <div className="products-container">
+      <h2 className="category-title">{category?.toUpperCase()}</h2>
+      <div className="product-category-list">
+        {products &&
+          products.map((product: WearData) => (
+            <Product key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/routes/Shop.tsx
+++ src/routes/Shop.tsx
@@ -1,31 +1,13 @@
-import { Fragment } from "react";
-import { useSelector } from "react-redux";
-import Product from "../parts/Product";
-import { WearData } from "../redux-components/categoriesSlice";
-import { RootState } from "../redux-components/store";
+import { Routes, Route } from "react-router-dom";
+import CategoryPreview from "../parts/CategoryPreview";
+import ProductCard from "./ProductCard";
 
 const Shop = () => {
-  const listOfProducts = useSelector(
-    (state: RootState) => state.categories.listOfProducts
-  );
-  console.log(listOfProducts);
-
   return (
-    <div className="products-container">
-      {listOfProducts &&
-        Object.keys(listOfProducts).map((title: string) => {
-          return (
-            <Fragment key={title}>
-              <h2 className="category-title">{title}</h2>
-              <div className="product-category-list">
-                {listOfProducts[title].map((product: WearData) => (
-                  <Product key={product.id} product={product} />
-                ))}
-              </div>
-            </Fragment>
-          );
-        })}
-    </div>
+    <Routes>
+      <Route index element={<CategoryPreview />} />
+      <Route path=":category" element={<ProductCard />} />
+    </Routes>
   );
 };
 
